bba2de3c8b5c083083e97031efd56467741c818e
tkucharski <tomasz.kucharski@coreservices.pl>
2019-09-14 17:39:21 +0200

    call find_face with buffer

--- server/app/src/app.py
+++ server/app/src/app.py
@@ -3,6 +3,9 @@ from flask import Flask, request
 import base64
 import remote
 import models
+import base64
+import io
+import utils
 
 app = Flask(__name__)
 
@@ -13,12 +16,17 @@ def authorize():
 
 
     remote.authorize(user.username, user.password)
-
-    filename = 'tmp_image.jpg'
-    with open(filename, 'wb') as f:
-        f.write(base64.b64decode(photo))
+    utils.find_face(decode_img(user.image))
 
     return 'success!'
+
+
+def decode_img(msg):
+    # msg = msg[msg.find(b"<plain_txt_msg:img>")+len(b"<plain_txt_msg:img>"):
+    #           msg.find(b"<!plain_txt_msg>")]
+    msg = base64.b64decode(msg)
+    buf = io.BytesIO(msg)
+    return buf
 
 
 if __name__ == '__main__':
