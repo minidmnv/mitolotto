003dc3abae7af74b9450ba01758b7f098a086dee
tkucharski <tomasz.kucharski@coreservices.pl>
2019-09-14 17:51:46 +0200

    remove unused method

--- server/app/src/app.py
+++ server/app/src/app.py
@@ -1,11 +1,10 @@
+import json
+
 from flask import Flask, request
 
-import remote
 import models
-import base64
-import io
+import remote
 import utils
-import json
 
 app = Flask(__name__)
 
@@ -19,12 +18,6 @@ def authorize():
     utils.find_face(buffer)
 
     return json.dumps({'authorized': True, 'details': 'successfull'})
-
-
-def decode_img(msg):
-    msg = base64.b64decode(msg)
-    buf = io.BytesIO(msg)
-    return buf
 
 
 if __name__ == '__main__':
