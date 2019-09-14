654f5424e24322fe7916675bab26205d3e0ff379
Szymon Wojciechowski <szymon.wojciechowski@coreservices.pl>
2019-09-14 17:21:59 +0200

    Changes to backend, added remote service and user moderl

--- server/app/src/app.py
+++ server/app/src/app.py
@@ -1,14 +1,19 @@
 from flask import Flask, request
+
 import base64
+import remote
+import models
 
 app = Flask(__name__)
 
 
 @app.route('/authorize', methods=['POST'])
 def authorize():
-    photo = request.json["photo"]
-    username = request.json["username"]
-    password = request.json["password"]
+    user = models.User(request)
+
+
+    remote.authorize(user.username, user.password)
+
     filename = 'tmp_image.jpg'
     with open(filename, 'wb') as f:
         f.write(base64.b64decode(photo))
