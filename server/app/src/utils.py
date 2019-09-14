94550b9440e330ed58c575798ddb3b7d01d42059
Szymon Wojciechowski <szymon.wojciechowski@coreservices.pl>
2019-09-14 21:50:08 +0200

    Fixes

--- server/app/src/utils.py
+++ server/app/src/utils.py
@@ -28,9 +28,9 @@ def find_face(image):
     raise TooManyFacesFoundError
 
 
-def compare_faces(known_image_path, unknown_image_path):
-    known_image = face_recognition.load_image_file(known_image_path)
-    unknown_image = face_recognition.load_image_file(unknown_image_path)
+def compare_faces(raw_known_image, raw_unknown_image):
+    known_image = face_recognition.load_image_file(raw_known_image)
+    unknown_image = face_recognition.load_image_file(raw_unknown_image)
 
     known_image_encoding = face_recognition.face_encodings(known_image)[0]
     unknown_image_encoding = face_recognition.face_encodings(unknown_image)[0]
