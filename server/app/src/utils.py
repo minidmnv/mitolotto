60de76580028c1067b7162958ad4d496d01112ca
Szymon Wojciechowski <szymon.wojciechowski@coreservices.pl>
2019-09-14 14:44:02 +0200

    Resizing image

--- server/app/src/utils.py
+++ server/app/src/utils.py
@@ -1,8 +1,16 @@
 import face_recognition
+from PIL import Image
+from exceptions import NoFaceFoundError
+from exceptions import TooManyFacesFoundError
+
+
+def resize_image(source_path, destination_path):
+    image = Image.open(source_path)
+    image.thumbnail((500, 500))
+    image.save(destination_path)
 
 
 def find_face(image):
-
     picture = face_recognition.load_image_file(image)
     picture_encodings = face_recognition.face_encodings(picture)
     found_faces = len(picture_encodings)
@@ -11,6 +19,11 @@ def find_face(image):
         return True
 
     if found_faces <= 0:
-        raise Exception('spam', 'eggs')
-    Exception('spam', 'eggs')
+        raise NoFaceFoundError
+    raise TooManyFacesFoundError
     pass
+
+
+def compare_faces(known_image, unkown_image):
+    pass
+
